a3388eeb0f01562a2dccd3f372381e453c281619
A9250916 <79899815+David2332-lin@users.noreply.github.com>
2023-06-06 13:38:42 +0800

    Update Gomoku.h

--- Gomoku.h
+++ Gomoku.h
@@ -8,7 +8,11 @@ public:
 	bool rowWin(char, int, int);	//判斷橫向棋子數 
 	bool obliWinl(char, int, int);  //判斷斜向棋子數
 	bool obliWin2(char, int, int);	//判斷斜向棋子數
-	
+	bool winner(char, int, int);	//上述四個判斷有一個成立,及獲勝
+	bool tfSet(char, int, int);		//輸入棋子位置
+	bool mapOver();					//搜尋是否滿盤,只要有一個是空的,地圖就不為滿
+	void show() const;				//顯示棋盤
+	void welcome() const;
 private:
 	char map[15][15];  // 地圖大小為15 * 15
 
